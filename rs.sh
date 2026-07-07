5dde7eeb0fd9ad9a1e317c0920c1c48c8e4b02d2
Daniel Einars <contact@dle.dev>
2026-07-07 23:05:21 +0200

    Update display scaling settings
    Changed:
    - Increase terminal and Xft sizing for the current display setup.
    - Add explicit xrandr scale arguments and move workspace 3 to HDMI-A-0.
    
    Removed:
    - Delete the stale Alacritty backup file.

--- rs.sh
+++ rs.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-1 --primary --mode 3840x2160 --pos 7680x0 --rotate normal --output DisplayPort-0 --mode 3840x2160 --pos 0x0 --rotate normal --output HDMI-A-0 --mode 3840x2160 --pos 3840x0 --rotate normal --output HDMI-A-1 --off
+xrandr --output DisplayPort-1 --primary --scale 1x1 --mode 3840x2160 --pos 7680x0 --rotate normal --output DisplayPort-0 --scale 1x1 --mode 3840x2160 --pos 0x0 --rotate normal --output HDMI-A-0 --scale 1x1 --mode 3840x2160 --pos 3840x0 --rotate normal --output HDMI-A-1 --off
